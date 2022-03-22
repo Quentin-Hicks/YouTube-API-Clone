8b2c9967b26ed3ebe253e0576dd6c48f0919418f
Quentin <qwhicks2@gmail.com>
2022-03-22 14:33:00 -0400

    Reinstall npm

--- backend/drf_jwt_backend/comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from comments import views
-
-urlpatterns = [
-    path('all/', views.comments_list),
-    path('<int:pk>/', views.user_comments)
-]
--- backend/drf_jwt_backend/reply/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from reply import views
-
-urlpatterns = [
-    path('<int:comment_id>/', views.user_reply),
-]
